ea15446eebd6001939587e4739cabd5ee68fa60d
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-04 14:22:33 -0300

    Add new fileds to Structure

--- src/app/_models/structure.ts
+++ src/app/_models/structure.ts
@@ -1,7 +1,12 @@
 export class Structure {
     name: string;
+    latitude: string;
+    longitude: string;
+    length: number;
+    width: number;
+    route: string;
+    km: number;
+    date_joined: Date;
+    photo: string;
     commentary: string;
-    create_date: Date;
-    latitude: number;
-    longitude: number;
 }
