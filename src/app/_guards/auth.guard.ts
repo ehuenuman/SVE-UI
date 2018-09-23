b6639485efcea3be9aba9dd7c5a1604a130e10d4
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-23 04:06:38 -0300

    Update canActive to use jwt

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,19 +1,19 @@
 ﻿import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, CanActivate } from '@angular/router';
+import { AuthenticationService } from '../_services/authentication.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-    constructor(private router: Router) { }
+  constructor(
+    private auth: AuthenticationService,
+    private router: Router) {}
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        if (localStorage.getItem('currentUser')) {
-            // logged in so return true
-            return true;
-        }
-
-        // not logged in so redirect to login page with the return url
-        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
-        return false;
+  canActivate() {
+    if (!this.auth.isLoggedIn()) {
+      this.router.navigateByUrl('/');
+      return false;
     }
+    return true;
+  }
 }
