a824f4558df6de3076b514cd5f773845b238114a
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-24 12:14:19 -0300

    Crea un arreglo de Estructuras, revisando si cada estructura posee alertas de advertencia y alertas, pasando la estructura a un solo objeto.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,8 +27,28 @@ export class HomeComponent implements OnInit {
         if (structures.error) {
           console.log("Error: ", structures.error )
         } else {
-          this.structures = structures.response[0];
-          console.log(this.structures);
+          structures.response[0].forEach(structure => {
+            if (this.structures.length == 0) {
+              this.structures.push(structure);
+            } else {
+              var id = structure.id;
+              var adv_count = structure.adv_count;
+              var ale_count = structure.ale_count;
+              var temp_structure = this.structures[this.structures.length-1];
+              //console.log(temp_structure);
+              if (temp_structure.id == id) {
+                //console.log("is same element");
+                if (temp_structure.adv_count > 0 && temp_structure.ale_count == 0) {
+                  this.structures[this.structures.length-1].ale_count = ale_count;
+                } 
+                if (temp_structure.adv_count == 0 && temp_structure.ale_count > 0) {
+                  this.structures[this.structures.length-1].adv_count = adv_count;
+                }
+              } else {
+                this.structures.push(structure);
+              }
+            }
+          })
         }
       },
       error => {
