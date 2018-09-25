5bc8937c4c52629dc7195b041ccb46ddb24aa6b5
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-24 23:51:24 -0300

    Create sensor model

---
+++ src/app/_models/sensor.model.ts
@@ -0,0 +1,18 @@
+export class Sensor {
+  id: number;
+  name: string;
+  installation_date: string;
+  latitude: string;
+  longitude: string;
+  photo: string;
+  commentary: string;
+  is_active: number;
+  calibrationsheet: string;
+  serial_number: string;
+  node_id: number;
+  type_id: number;
+  type_sensor_name: string;
+  type_sensor_model: string;
+  type_sensor_unit: string;
+  type_sensor_datasheet: string;
+}
