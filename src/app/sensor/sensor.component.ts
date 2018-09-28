8b0e4bf398bb5d06826f0c78895373b16e48499b
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-28 11:14:11 -0300

    Create Sensor component

--- src/app/sensor/sensor.component.ts
+++ src/app/sensor/sensor.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { SensorService } from '../_services';
+
+import { Sensor } from '../_models';
 
 @Component({
   selector: 'app-sensor',
@@ -6,10 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sensor.component.css']
 })
 export class SensorComponent implements OnInit {
+  sensor: Sensor;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private sensorService: SensorService
+  ) { }
 
   ngOnInit() {
+    this.getSensor();
   }
 
+  getSensor(): void {
+    const structure_id = +this.route.snapshot.paramMap.get('structure_id');
+    const sensor_id = +this.route.snapshot.paramMap.get('sensor_id');    
+    this.sensorService.getSensor(structure_id, sensor_id)
+      .subscribe(sensor => this.sensor = sensor.response)
+  }
 }
